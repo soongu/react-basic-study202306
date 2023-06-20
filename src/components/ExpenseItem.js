7bd62ef95b4c40ed66c80fa11d81e48fe166bfe2
soongu <hsg9984@gmail.com>
2023-06-20 16:38:20 +0900

    #5. props2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,22 +3,22 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  console.log(props);
+  // console.log(props);
 
-  const makeFormattedDate = () => {
-    const year = props.date.getFullYear();
-    const month = props.date.getMonth(); // 자바스크립트에서는 월이 0부터 시작합니다.
-    const date = props.date.getDate();
-
-    return `${year}년 ${month.toString().padStart(2, "0")}월 ${date.toString().padStart(2, "0")}일`;
-  };
+  const year = props.date.getFullYear();
+  const month = props.date.getMonth();
+  const day = props.date.getDay().toString().padStart(2, '0');
 
 
   const formattedPrice = new Intl.NumberFormat("ko-KR").format(props.price);
 
   return (
     <div className="expense-item">
-      <div>{makeFormattedDate()}</div>
+      <div>
+        <div>{year}</div>
+        <div>{month}월</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
