6df750f93be1698c0baa845af4ca2cbdc0ec0456
soongu <hsg9984@gmail.com>
2023-06-20 15:23:22 +0900

    #4. props1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: 1, title: "냠냠치킨", price: 19000, date: new Date(2023, 6, 19) },
+    { id: 2, title: "양파", price: 5000, date: new Date(2023, 6, 20) },
+    { id: 3, title: "포도", price: 20000, date: new Date(2023, 6, 21) },
+    { id: 4, title: "오렌지", price: 15000, date: new Date(2023, 6, 22) },
+  ];
+
   return (
     <div>
       <h2>리액트 시작하기</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
