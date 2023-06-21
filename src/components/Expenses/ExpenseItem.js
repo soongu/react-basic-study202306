2f799418bb0975a25668732bb24c98aff55cd608
soongu <hsg9984@gmail.com>
2023-06-21 17:23:58 +0900

    #8. props.children

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(props.price);
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
