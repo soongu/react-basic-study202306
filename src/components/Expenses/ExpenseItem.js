85daaa94ef3d89f85ed70f4bfb8ec9fa36bd2b14
soongu <hsg9984@gmail.com>
2023-06-22 14:33:14 +0900

    #9. useState 기초

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,31 @@
-import React from "react";
+
+
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
+  
+  // let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(props.price);
-
+  
+  const clickHandler = () => {
+    // console.log('click!!!');
+    // title = 'gggg';
+    setTitle('gggg');
+  };
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
       </div>
+      <button onClick={clickHandler}>수정</button>
     </Card>
   );
 };
