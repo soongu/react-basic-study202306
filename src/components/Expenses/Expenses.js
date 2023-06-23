20a77e62d4d3bcb52886e56c63802efe952609fb
soongu <hsg9984@gmail.com>
2023-06-23 14:17:30 +0900

    #13. 과제 - 선택한 연도를 Expenses로 끌어올려보세요

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,44 @@
-
-import React from 'react';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <div className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        price={props.items[0].price}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        price={props.items[1].price}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        price={props.items[2].price}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        price={props.items[3].price}
-        date={props.items[3].date}
-      />
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          price={props.items[0].price}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          price={props.items[1].price}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          price={props.items[2].price}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          price={props.items[3].price}
+          date={props.items[3].date}
+        />
+      </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
