e5aa52b9735a6b3057aa26aeb8e32ab6007e612c
soongu <hsg9984@gmail.com>
2023-06-25 14:13:24 +0900

    #32. React.Fragment

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Card from './Card';
-import Button from './Button';
-import styles from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import styles from "./ErrorModal.module.css";
 
-const ErrorModal = ({title, message, onConfirm}) => {
+const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-    <div className={styles.backdrop} onClick={onConfirm} />
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>{title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{message}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button onClick={onConfirm}>Okay</Button>
-      </footer>
-    </Card>
+      <div className={styles.backdrop} onClick={onConfirm} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={onConfirm}>Okay</Button>
+        </footer>
+      </Card>
     </>
   );
 };
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -51,7 +51,7 @@ const AddUser = ({ onAddUser }) => {
     setError(null);
   };
   return (
-    <>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -78,7 +78,7 @@ const AddUser = ({ onAddUser }) => {
           <Button type="submit">가입하기</Button>
         </form>
       </Card>
-    </>
+    </React.Fragment>
   );
 };
 
