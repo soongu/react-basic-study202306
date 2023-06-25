c955b0fd0d9e947de2058f1f558e299526ebb11f
soongu <hsg9984@gmail.com>
2023-06-25 13:34:31 +0900

    #30. 연습프로젝트 - 에러 모달 만들기

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,8 +4,9 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import styles from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = ({onAddUser}) => {
+const AddUser = ({ onAddUser }) => {
   const [userValue, setUserValue] = useState({
     username: "",
     age: "",
@@ -13,15 +14,15 @@ const AddUser = ({onAddUser}) => {
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    const {username, age} = userValue;
+    const { username, age } = userValue;
     if (username.trim().length === 0 || age.trim().length === 0) {
       return;
     }
-    if(+age < 1) return;
-    
+    if (+age < 1) return;
+
     onAddUser(userValue);
     // console.log(userValue.username, userValue.age);
-    setUserValue({ username: '', age: '' });
+    setUserValue({ username: "", age: "" });
   };
 
   const usernameChangerHandler = (e) => {
@@ -36,25 +37,28 @@ const AddUser = ({onAddUser}) => {
   };
 
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">이름</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangerHandler}
-          value={userValue.username}
-        />
-        <label htmlFor="age">나이</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={userValue.age}
-        />
-        <Button type="submit">가입하기</Button>
-      </form>
-    </Card>
+    <>
+      <ErrorModal title={"제목이 비었네용"} message={"공백은안됩니디~~"} />
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">이름</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangerHandler}
+            value={userValue.username}
+          />
+          <label htmlFor="age">나이</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={userValue.age}
+          />
+          <Button type="submit">가입하기</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
