c34cb01f57ff522e7a76434fcfa4530762bf14b2
soongu <hsg9984@gmail.com>
2023-06-26 23:34:05 +0900

    #48. 연습프로젝트2 - 음식리스트 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <>
       <Header />
+      <div id='main'>
+        <Meals />
+      </div>
     </>
   );
 };
