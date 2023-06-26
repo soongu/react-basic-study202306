ec0cbc471988f80c0b0fec3846bc76e586e3d85e
soongu <hsg9984@gmail.com>
2023-06-27 01:40:54 +0900

    #54. 연습프로젝트2 - 장바구니 리듀서 추가

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = ({ children }) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalPrice: 0
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const addedItems = [...state.items, action.item];
+    const updatedPrice = state.totalPrice + action.item.amount * action.item.price;
+    return {
+      items: addedItems,
+      totalPrice: updatedPrice
+    };
+  } else if (action.type === 'REMOVE') {
+    const removedItems = state.items.filter(item => item.id !== action.id);
+    return {
+      items: removedItems,
+      totalPrice: state.totalPrice
+    };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = ({ children }) => {
+
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalPrice: 0,
+    items: cartState.items,
+    totalPrice: cartState.totalPrice,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
