ee446b076608c6c84e7d6e39f457183a20765c66
soongu <hsg9984@gmail.com>
2023-06-27 01:25:44 +0900

    #53. 연습프로젝트2 - 장바구니 수량 배지 처리

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "./HeaderCartButton.module.scss";
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
   const { button, icon, badge } = styles;
+  const { items } = useContext(CartContext);
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0);
+
   return (
     <button className={button} onClick={onClick}>
       <span className={icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      <span className={badge}>{numberOfCartItems}</span>
     </button>
   );
 };
