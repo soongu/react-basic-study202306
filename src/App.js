2f9153b913cbadff723c9d05a2b2bcac278a8f7a
soongu <hsg9984@gmail.com>
2023-06-26 13:15:11 +0900

    #40. Context API 기초 - Provider와 Consumer사용하기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/SideEffect/Login/Login';
-import Home from './components/SideEffect/Home/Home';
-import MainHeader from './components/SideEffect/MainHeader/MainHeader';
+import Login from "./components/SideEffect/Login/Login";
+import Home from "./components/SideEffect/Home/Home";
+import MainHeader from "./components/SideEffect/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    console.log('useEffect 실행!!');
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    console.log("useEffect 실행!!");
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLoggedInInformation === '1') {
+    if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
 
   const loginHandler = (email, password) => {
-    localStorage.setItem('isLoggedIn', '1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
-    <>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/SideEffect/Home/Home.js
+++ src/components/SideEffect/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Card from '../../UI/Card';
 import styles from './Home.module.css';
+import Button from '../../UI/Button';
 
-const Home = () => {
+const Home = ({onLogout}) => {
   return (
     <Card className={styles.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Logout</Button>
     </Card>
   );
 };
