e0b47f7f3d192ff75964faa9cfa7fe6766252691
soongu <hsg9984@gmail.com>
2023-06-27 14:07:03 +0900

    #56. 연습프로젝트2 - 장바구니 항목 렌더링하기

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "./Cart.module.scss";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = ({ onClose }) => {
   const {
-    'cart-items': cartItemsStyle,
+    "cart-items": cartItemsStyle,
     total,
     actions,
     button,
-    'button--alt': btnAlt,
+    "button--alt": btnAlt,
   } = styles;
+
+  const { items, totalPrice, addItem, removeItem } = useContext(CartContext);
+
+  const formatTotalPrice = new Intl.NumberFormat("ko-KR").format(totalPrice);
+
+  const hasItems = items.length > 0;
+
+  const cartAddHandler = item => {
+    addItem({...item, amount: 1});
+  };
+  const cartRemoveHandler = id => {
+    removeItem(id);
+  };
 
   const cartItems = (
     <ul className={cartItemsStyle}>
-      {[{ id: "c1", name: "스시", amount: 2, price: 50000 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {items.map((item) => (
+        <CartItem
+          key={item.id}
+          cart={item}
+          onRemove={cartRemoveHandler.bind(null, item.id)}
+          onAdd={cartAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -25,11 +45,13 @@ const Cart = ({ onClose }) => {
       {cartItems}
       <div className={total}>
         <span>주문 총액</span>
-        <span>50,000원</span>
+        <span>{formatTotalPrice}원</span>
       </div>
       <div className={actions}>
-        <button className={btnAlt} onClick={onClose}>닫기</button>
-        <button className={button}>주문</button>
+        <button className={btnAlt} onClick={onClose}>
+          닫기
+        </button>
+        {hasItems && <button className={button}>주문</button>}
       </div>
     </Modal>
   );
