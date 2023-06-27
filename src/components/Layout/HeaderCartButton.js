7a63a8ce33f7df470bcc93f03072982133776cc8
soongu <hsg9984@gmail.com>
2023-06-27 16:21:52 +0900

    #58. 연습프로젝트2 - useEffect로 HeaderCartButton에 애니메이션

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import styles from "./HeaderCartButton.module.scss";
 
@@ -6,15 +6,33 @@ import CartIcon from '../Cart/CartIcon';
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
-  const { button, icon, badge } = styles;
+  const [btnIsHighlighed, setBtnIsHighlighed] = useState(false);
   const { items } = useContext(CartContext);
+  const { button, icon, badge, bump } = styles;
 
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount
   }, 0);
 
+  const btnClassName = `${button} ${btnIsHighlighed ? bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsHighlighed(true);
+
+    // 애니메이션 지속시간이 300밀리초라 재생 후에 지워줌
+    const timer = setTimeout(() => {
+      setBtnIsHighlighed(false);
+    }, 300);
+
+    // 엄청빠르게 누르면 애니메이션이 중첩됨 그걸 방지
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={button} onClick={onClick}>
+    <button className={btnClassName} onClick={onClick}>
       <span className={icon}>
         <CartIcon />
       </span>
