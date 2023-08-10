28cd515bc2ab26a91d37c647f62d965f6af3faae
soongu <naong526@naver.com>
2023-08-10 21:32:10 +0900

    #59. 컴포넌트 업데이트 상태

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from './components/Meals/Meals';
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
+
+import './App.css';
 
 const App = () => {
 
-  // 장바구니 모달 열고닫는 상태변수
-  const [cartIsShown, setCartIsShown] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
+  // state가 변경되면 이 로그가 계속 찍힘
+  // 그 말은 곧 상태가 변경되면 컴포넌트 전체가 재평가된다는 소리
+  console.log('App 실행!');
 
-  const hideCartHandler = () => {
-    setCartIsShown(false);
+  const toggleParagraphHandler = e => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
 
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <div id='main'>
-        <Meals />
-      </div>
-    </CartProvider>
+    <div className="app">
+      <h1>방가방가</h1>
+      {showParagraph && <p>새로운 문단~~</p>}
+      <Button onClick={toggleParagraphHandler}>문단 보이기</Button>
+    </div>
   );
-};
+}
 
 export default App;
--- src/components/UI/Portal.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const Portal = ({ children, target }) => {
-  return ReactDOM.createPortal(children, document.getElementById(target));
-};
-
-export default Portal;
